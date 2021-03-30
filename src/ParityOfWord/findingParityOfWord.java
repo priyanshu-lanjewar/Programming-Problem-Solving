3e529eaf43fc881901350ada779bc25357003bc3
priyanshu <priyanshu.lanjewar@yahoo.com>
2021-03-30 13:54:54 +0530

    Parity of Word

---
+++ src/ParityOfWord/findingParityOfWord.java
@@ -0,0 +1,23 @@
+package ParityOfWord;
+
+import java.util.Scanner;
+
+public class findingParityOfWord {
+    public static short parity(long x) {
+        short result = 0;
+        while (x != 0) {
+            result ^= (x & 1);
+            x >>>= 1;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int number = scanner.nextInt();
+
+        System.out.println("Parity = " + parity(number));
+    }
+}
+
---
+++ src/ParityOfWord/readme.md
@@ -0,0 +1,3 @@
+# Parity Of Word
+### If number of 1's in given binary data is even then parity = 0
+### If number of 1's in given binary data is odd then parity = 1
