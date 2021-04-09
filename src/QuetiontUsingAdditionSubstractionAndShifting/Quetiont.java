b19b9279e838ed9c8fa834d48f5579dd40492d1c
priyanshu <priyanshu.lanjewar@yahoo.com>
2021-04-09 14:20:30 +0530

    X / Y

---
+++ src/QuetiontUsingAdditionSubstractionAndShifting/Quetiont.java
@@ -0,0 +1,33 @@
+package QuetiontUsingAdditionSubstractionAndShifting;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Quetiont {
+    public static long divide(long x, long y) {
+        long result = 0;
+        int power = 32;
+        long yPower = y << power;
+        while (x >= y) {
+            while (yPower > x) {
+                yPower >>>= 1;
+                --power;
+            }
+
+            result += 1L << power;
+            x -= yPower;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter X : ");
+        long X = Long.parseLong(reader.readLine());
+        System.out.print("Enter Y : ");
+        long Y = Long.parseLong(reader.readLine());
+        System.out.println("Value Of X/Y : " + divide(X, Y));
+    }
+}
---
+++ src/QuetiontUsingAdditionSubstractionAndShifting/readme.md
@@ -0,0 +1,14 @@
+# COMPUTATION of  x/y
+
+## Problem Statement :Given two positive integers, compute their quotient, using only the addition, subtraction, and shifting operators.
+
+
+## Testcase
+### Input :
+1024<br>
+2
+
+### Output : 
+512
+
+##In essence, the program applies the grade-school division algorithm to binary numbers. With each iteration, we process an additional bit. Therefore, assuming individual shift and add operations take 0(1) time, the time complexity is 0(n).
